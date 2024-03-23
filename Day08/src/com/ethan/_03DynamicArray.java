2c9ad9aa8209ce17a7da54d538abbb04892f00c2
ethanliu6 <liue2336@gmail.com>
2024-03-23 14:41:46 +0800

    Day08上午案例优化 3.23 格式化打印

--- Day08/src/com/ethan/_03DynamicArray.java
+++ Day08/src/com/ethan/_03DynamicArray.java
@@ -14,12 +14,28 @@ public class _03DynamicArray {
 
         //遍历键盘输入并加入数组
         for (int i = 0; i < arr.length; i++) {
-            System.out.print("请输入第" + (i+1) + "个数:");
+            System.out.print("请输入第" + (i + 1) + "个数:");
             arr[i] = input.nextInt();
         }
+        //格式化打印数组
+        printRst(arr);
 
+    }
+
+    public static void printRst(int[] arr) {
+        /*
+            res: arr = [1, 2, 3, 4, 5]
+         */
+        System.out.print("arr = [");
+        //遍历数组
         for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
+            if (i != (arr.length - 1)) { // 最后一位不需要", "
+                System.out.print(arr[i] + ", ");
+            } else {
+                System.out.print(arr[i]);
+            }
         }
+        System.out.println("]");
+
     }
 }
