8757928e32dccf168d7f1136e5f5f36eec368701
ethanliu6 <liue2336@gmail.com>
2024-03-23 09:58:47 +0800

    考勤的案例已完成 3.23

--- Day07/src/com/ethan/_05_KaoQinDate.java
+++ Day07/src/com/ethan/_05_KaoQinDate.java
@@ -1,12 +1,16 @@
 package com.ethan;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class _05_KaoQinDate {
     public static void main(String[] args) {
 //      日期控件对象(静态方法)
-        DateChooser chooser1 = DateChooser.getInstance("yyyy/MM/dd");
-        DateChooser chooser2 = DateChooser.getInstance("yyyy/MM/dd");
+        DateChooser chooserStart = DateChooser.getInstance("yyyy-MM-dd");
+        DateChooser chooserEnd = DateChooser.getInstance("yyyy-MM-dd");
 
 //      考勤面板
         JFrame jf = new JFrame("考勤日期查询");
@@ -33,14 +37,14 @@ public class _05_KaoQinDate {
 //      开始时间组件
         JTextField startField = new JTextField();
         startField.setBounds(50, 100, 100, 20);
-        chooser1.register(startField); // 开始时间选择组件
+        chooserStart.register(startField); // 开始时间选择组件
         jf.add(startField);
 
 //      结束时间组件
-        JTextField textField2 = new JTextField();
-        textField2.setBounds(250, 100, 100, 20);
-        chooser2.register(textField2);  // 结束时间选择组件
-        jf.add(textField2);
+        JTextField endField = new JTextField();
+        endField.setBounds(250, 100, 100, 20);
+        chooserEnd.register(endField);  // 结束时间选择组件
+        jf.add(endField);
 
 //      确定按钮组件
         JButton btn = new JButton("确定");
@@ -48,11 +52,29 @@ public class _05_KaoQinDate {
         jf.add(btn);
 
 
-        //考勤的Date和今日的结合案例未完成
+//      添加按钮事件
+        btn.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // 获取str日期
+                String strStart = startField.getText();
+                String strEnd = endField.getText();
 
+                // 格式化日期对象
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                // 解析为Date格式
+                try {
+                    Date startDate = sdf.parse(strStart);
+                    Date endDate = sdf.parse(strEnd);
 
+                    // 弹窗展示
+                    JOptionPane.showMessageDialog(jf, startDate + "\n" + endDate);
 
-
+                } catch (ParseException parseException) {
+                    parseException.printStackTrace();
+                }
+            }
+        });
 
 
         jf.setVisible(true);
